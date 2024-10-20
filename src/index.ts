480590610dccc4d85b663c61236e26b61a9fca73
Mathieu Marques <mathieumarques78@gmail.com>
2024-10-20 22:38:32 +0200

    feat(karnnect): re-subscribe automatically

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { scheduleJob } from 'node-schedule';
+
 import { Bot } from './bot';
 import { karnnect } from './karnnect';
 import { Server } from './server';
@@ -6,6 +8,11 @@ import { Server } from './server';
   await Promise.all([Bot.start(), Server.start()]);
   karnnect();
 })();
+
+// NOTE Since YouTube WebSub subscriptions are limited in leasing time, run the
+//      subscription routine every Saturday at 10:00.
+//      See `karnnect` for the implementation details.
+scheduleJob('0 10 * * 6', karnnect);
 
 process.on('SIGINT', () => Promise.all([Bot.stop(), Server.stop()]));
 process.on('SIGTERM', () => Promise.all([Bot.stop(), Server.stop()]));
--- src/karnnect.ts
+++ src/karnnect.ts
@@ -1,3 +1,5 @@
+import { Bot } from './bot';
+
 const CHANNELS = {
   PirateSoftware: 'UCMnULQ6F6kLDAHxofDWIbrw',
   t3dotgg: 'UCbRP3c757lWg9M-U7TyEkXA',
@@ -5,33 +7,37 @@ const CHANNELS = {
 
 const URLS = {
   CALLBACK: 'https://diskarn.up.railway.app/challenge',
-  HUB: 'https://pubsubhubbub.appspot.com',
+  DIAGNOSE: 'https://pubsubhubbub.appspot.com/subscription-details',
+  SUBSCRIBE: 'https://pubsubhubbub.appspot.com',
   TOPIC: 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=',
 } as const;
 
+/**
+ * Subscribe to the WebSub notifications all of the channels in sequence.
+ * Await in each iteration as a cheap way not to care about rate limits.
+ * See <https://developers.google.com/youtube/v3/guides/push_notifications>.
+ */
 export const karnnect = async () => {
+  const total = Object.keys(CHANNELS).length;
   let index = 0;
   for (const [name, id] of Object.entries(CHANNELS)) {
-    const total = Object.keys(CHANNELS).length;
     console.info(`[karnnect] Subscribing ${++index}/${total} "${name}"...`);
     if (process.env.SKIP_SUBSCRIPTION === '1') {
       console.info(`[karnnect] Subscribed "${name}" (fake)`);
       continue;
     }
-    // TODO Refresh subscriptions automatically
-    // NOTE Await in between as a cheap way not to care about rate limits
-    const response = await subscribe({ id });
-    if (!response.ok) {
-      throw new Error(`${response.status} ${response.statusText}`);
-    }
+    const lease = 10 * 24 * 60 * 60;
+    const { ok, status, statusText } = await subscribe({ id, lease });
+    if (!ok) throw new Error(`${status} ${statusText}`);
     console.info(`[karnnect] Subscribed "${name}"`);
   }
+  Bot.log(
+    `# Subscribed ${total} WebSub notifications`,
+    ...Object.entries(CHANNELS).map(([name]) => `- \`${name}\``),
+  );
 };
 
-/**
- * Subscribe or unsubscribe to a specific channel WebSub notifications.
- * See https://developers.google.com/youtube/v3/guides/push_notifications.
- */
+/** Subscribe or unsubscribe to a specific channel WebSub notifications */
 const subscribe = (options: {
   /** The ID of the YouTube channel to subscribe to */
   id: string;
@@ -46,12 +52,12 @@ const subscribe = (options: {
   /** Verify type */
   verify?: 'async' | 'sync';
 }) =>
-  fetch(URLS.HUB, {
+  fetch(URLS.SUBSCRIBE, {
     body: new URLSearchParams({
       'hub.callback': URLS.CALLBACK,
       'hub.mode': options.undo ? 'unsubscribe' : 'subscribe',
       'hub.topic': `${URLS.TOPIC}${options.id}`,
-      ...(options.lease && { ['hub.lease_numbers']: `${options.lease}` }),
+      ...(options.lease && { ['hub.lease_seconds']: `${options.lease}` }),
       ...(options.secret && { ['hub.secret']: options.secret }),
       ...(options.token && { ['hub.verify_token']: options.token }),
       ...(options.verify && { ['hub.verify']: options.verify }),
@@ -59,3 +65,23 @@ const subscribe = (options: {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     method: 'POST',
   });
+
+/**
+ * Inspect the current state for the provided subscription.
+ * Unfortunately YouTube's implementation is not WebSub-compliant and in
+ * addition to return nothing on subscription, yields HTML instead of JSON for
+ * the diagnostic endpoint.
+ */
+const diagnose = (options: {
+  /** The ID of the YouTube channel to diagnose */
+  id: string;
+  /** HMAC secret */
+  secret?: string;
+}) => {
+  const query = new URLSearchParams({
+    'hub.callback': URLS.CALLBACK,
+    'hub.topic': `${URLS.TOPIC}${options.id}`,
+    ...(options.secret && { ['hub.secret']: options.secret }),
+  });
+  return fetch(`${URLS.DIAGNOSE}?${query.toString()}`);
+};
