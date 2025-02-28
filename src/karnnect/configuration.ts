ba66ffd25b6f7378395e2173748929ae20c8bcf8
Mathieu Marques <mathieumarques78@gmail.com>
2025-02-28 11:49:10 +0100

    feat(karnnect): plug Doomsday server again

--- src/karnnect/configuration.ts
+++ src/karnnect/configuration.ts
@@ -29,7 +29,7 @@ export const CONFIGURATION: {
 }[] = [
   {
     filter: /[\w-]day|ddft|tainted/i,
-    server: 'KORUMITE',
+    server: 'DOOMSDAY',
     subscriptions: [
       SUBSCRIPTIONS.DDFT_GUY,
       SUBSCRIPTIONS.DOISHY,
